c0a3af8f95804c796eb0af11d94fcb1d616a1df4
JonnyKong <jonnykong1996@gmail.com>
2020-01-02 14:28:28 +0800

    lab06: update

---
+++ lab06_nonlocal_and_object_oriented_programming/lab06.py
@@ -0,0 +1,24 @@
+
+def make_adder_inc(n):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2) 
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    def adder_inc(x):
+        nonlocal base
+        base += 1
+        return n + x + base - 1
+
+    base = 0
+    return adder_inc
